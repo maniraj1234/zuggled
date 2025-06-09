01a7759c2b78b40746fec6eabba217f0d4068a8a
Crystal <150055488+crystalarcus@users.noreply.github.com>
2025-06-09 22:26:49 +0530

    static homescreen & layout changes (#15)
    * firebase configured for flutter, phone otp verification has been added
    
    * Added HomeScreen, Navigation Bar and setup Router Config for all main screens with ShellRouter
    
    * Update build.gradle.kts
    
    Added TODO
    
    * Added docstrings and kDebugMode check to print statements
    
    * Added docstrings to rest of the Widgets and Classes (Except files for Backend) (#16)
    
    * HomeScreen UI update
    
    * OnBoardingScreen UI updates and set up OnBoardingScreen as once only
    
    ---------
    
    Co-authored-by: tsumith <tsumith987@gmail.com>

--- frontend/lib/core/navigation/main_router.dart
+++ frontend/lib/core/navigation/main_router.dart
@@ -1,7 +1,16 @@
+import 'package:frontend/core/navigation/root_nav_screen.dart';
+import 'package:frontend/features/auth_test/view/login.dart';
 import 'package:frontend/features/calling/pages/call_feature.dart';
+// ignore: unused_import
 import 'package:frontend/features/onboarding/onboarding.dart';
+import 'package:frontend/views/coins_screen.dart';
+import 'package:frontend/views/history_screen.dart';
+import 'package:frontend/views/home_screen.dart';
+import 'package:frontend/views/settings_screen.dart';
+import 'package:frontend/widgets/transition_page.dart';
 import 'package:go_router/go_router.dart';
 import 'package:frontend/core/navigation/nav_error_handler.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 /// Defines the main **Application router**
 ///
@@ -14,21 +23,86 @@ import 'package:frontend/core/navigation/nav_error_handler.dart';
 ///   routerConfig: router,
 /// );
 /// ```
+
+/// To check is this is app's launch
+bool isAppLaunch = true;
+
 final GoRouter mainRouter = GoRouter(
   /// [debugLogDiagnostics] set to true for showing debug logs
   debugLogDiagnostics: true,
 
+  /// ***Check for First Launch***
+  /// If first launch, redirect to onboardingScreen
+  redirect: (context, state) async {
+    if (isAppLaunch) {
+      isAppLaunch = false;
+    } else {
+      return null;
+    }
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (prefs.getBool('isFirstLaunch') ?? true) {
+      return '/onBoarding';
+    } else {
+      return '/home';
+    }
+  },
+
   /// **Initial Route**
   ///
   /// Defines the default screen to show when the app starts.
-  initialLocation: '/',
+  initialLocation: '/home',
 
   /// **Application Routes**
   ///
   /// - [`OnboardingScreen`] serves as the main view (for testing purposes).
   /// - Additional screens can be added here for modular navigation.
   routes: [
-    GoRoute(path: '/', builder: (context, state) => OnboardingScreen()),
+    //TODO:Change HomeScreen to OnboardingScreen()
+    /// ShellRoute will be RootScreen and act as container for Other main screens like
+    /// HomeScreen, SettingScrren, HistoryScreen
+    /// and have a NavigationBar persistant across all these screens.
+    GoRoute(
+      path: '/onBoarding',
+      builder: (context, state) => OnboardingScreen(),
+    ),
+    GoRoute(path: '/login', builder: (context, state) => LoginPage()),
+    ShellRoute(
+      pageBuilder:
+          (context, state, child) =>
+              NoTransitionPage(child: RootNavScreen(child: child)),
+      routes: [
+        /// HomeScreen
+        GoRoute(
+          path: '/home',
+          pageBuilder:
+              (context, state) =>
+                  TransitionPage(child: HomeScreen(), state: state),
+        ),
+
+        GoRoute(
+          path: '/history',
+          pageBuilder:
+              (context, state) =>
+                  TransitionPage(child: HistoryScreen(), state: state),
+        ),
+
+        GoRoute(
+          path: '/coins',
+          pageBuilder:
+              (context, state) =>
+                  TransitionPage(child: CoinsScreen(), state: state),
+        ),
+
+        GoRoute(
+          path: '/settings',
+          pageBuilder:
+              (context, state) =>
+                  TransitionPage(child: SettingsScreen(), state: state),
+        ),
+      ],
+    ),
+    // TODO: Redesign Call Screen UI
+    /// CallScreen
     GoRoute(
       path: '/call_screen',
       builder: (context, state) => CallScreen(title: "call home screen"),
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,13 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:frontend/core/app_root.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  // This is to make status bar adjust according to app's theme.
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.black, // Example color
+      statusBarIconBrightness: Brightness.light,
+      statusBarBrightness: Brightness.dark,
+    ),
+  );
+
   ///**App Entry Point**
   ///
   ///
   ///load environment variables from .env file
-  await dotenv.load(fileName: "lib/core/config/.env");
+  await dotenv.load(fileName: "lib/core/config/template.env");
 
   ///[AppRoot] is the entry component of the application
   runApp(AppRoot());
