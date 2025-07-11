71c034ca295b2762b9504b39821a2bf16457cfd0
tsumith <tsumith987@gmail.com>
2025-07-11 20:31:01 +0530

    added user schema

--- backend/functions/src/validators/creator.schema.ts
+++ backend/functions/src/validators/creator.schema.ts
@@ -1,16 +1,10 @@
+import { userSchema } from './user.schema';
 import { z } from 'zod';
 
-export const userSchema = z.object({
-    userName: z.string().min(1, 'Username is required'),
-    bio: z.string().optional().default(''),
-    gender: z.enum(['male', 'female']),
-    birthdate: z.string().min(1, 'Birthdate is required'),
-    profilePictureURL: z.string().url('Must be a valid URL'),
-    interests: z.array(z.string()).optional().default([]),
-    phoneNumber: z
-        .string()
-        .regex(/^\+?[0-9]{10,15}$/, 'Phone number must be valid'),
-    coins: z.number().int().nonnegative().default(0),
+export const creatorSchema = userSchema.extend({
+    videoCallAvailable: z.boolean().default(false),
+    audioCallAvailable: z.boolean().default(false),
+    total_earnings: z.number().nonnegative().default(0),
 });
 
-export type UserInput = z.infer<typeof userSchema>;
+export type CreatorInput = z.infer<typeof creatorSchema>;
